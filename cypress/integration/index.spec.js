784a1eb2fb7fc0a0b15f0b13457d6b6ed6ec0a6c
Uchenna NnamadimUchenna NnamadimUchenna NnamadimUchenna Nnamadim <unnamadim@gmail.com>
2021-02-21 08:26:35 +0100

    added an assertion to my first test

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -3,5 +3,6 @@
 describe('User Interface test automation for Bitfinex website', () => {
     it('should visit the bitfinex website', () => {
         cy.visit('https://www.bitfinex.com/')
+        cy.title().should('include', 'Bitfinex')
     })
 })
