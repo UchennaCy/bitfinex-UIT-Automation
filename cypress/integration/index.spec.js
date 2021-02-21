6a4491717283a96aa6b3a9a8b037aeaa577a6db5
Uchenna NnamadimUchenna NnamadimUchenna NnamadimUchenna Nnamadim <unnamadim@gmail.com>
2021-02-21 08:32:36 +0100

    added test to search for Unus Sed Leo

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -9,4 +9,8 @@ describe('User Interface test automation for Bitfinex website', () => {
     it('should click on the ticker search icon', () => {
         cy.get('.bp3-tab-list > .landing-tickers__search > .bp3-icon > svg').click()
     })
+
+    it('should search for Unus Sed Leo', () => {
+        cy.get('.visible-desktop > .bp3-input-group > .bp3-input').type('Unus Sed Leo{enter}')
+    })
 })
