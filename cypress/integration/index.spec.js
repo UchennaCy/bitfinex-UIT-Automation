09d34fd858769e8bb458d89befff451ce136bcd8
Uchenna NnamadimUchenna NnamadimUchenna NnamadimUchenna Nnamadim <unnamadim@gmail.com>
2021-02-21 08:27:44 +0100

    added test to click on the ticker search icon

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -5,4 +5,8 @@ describe('User Interface test automation for Bitfinex website', () => {
         cy.visit('https://www.bitfinex.com/')
         cy.title().should('include', 'Bitfinex')
     })
+
+    it('should click on the ticker search icon', () => {
+        cy.get('.bp3-tab-list > .landing-tickers__search > .bp3-icon > svg').click()
+    })
 })
