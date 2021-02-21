8d35b00e263698eb1c213320e139b7de881a6ab4
Uchenna NnamadimUchenna NnamadimUchenna NnamadimUchenna Nnamadim <unnamadim@gmail.com>
2021-02-21 08:19:51 +0100

    configured my script in package.json

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,7 +1,5 @@
 /// < reference types="Cypress"/>
 
-const { describe } = require("mocha");
-
 describe('User Interface test automation for Bitfinex website', () => {
     it('should visit the bitfinex website', () => {
         cy.visit('https://www.bitfinex.com/')
